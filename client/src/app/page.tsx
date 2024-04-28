b655b6d82fad0cbaba6d1413f51ebcf1eb9e1024
Kevin Wu <kevinwu098@gmail.com>
2024-04-27 21:01:21 -0700

    feat: initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main className="wrapper flex-center min-h-[calc(100vh-6rem)] text-3xl font-semibold md:text-5xl">
+            Hello World 💖
+        </main>
+    );
+}
