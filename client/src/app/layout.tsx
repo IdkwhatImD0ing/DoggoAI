c64b659ca2f2a2adf6819b943e24c6f281e50958
Kevin Wu <kevinwu098@gmail.com>
2024-04-27 22:59:10 -0700

    feat: create navbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
-
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 export const metadata: Metadata = {
     title: "DoggoAI",
     description: "DoggoAI - HackDavis 2024",
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={cn(poppins.className, "flex min-h-[100vh]")}>
+                <Navbar />
+                {children}
+            </body>
         </html>
     );
 }
