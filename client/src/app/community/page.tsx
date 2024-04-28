bd5bef9f4e76b79d88ff18123abc3c482106181d
Kevin Wu <kevinwu098@gmail.com>
2024-04-28 09:32:34 -0700

    feat: add video, modal component

--- client/src/app/community/page.tsx
+++ client/src/app/community/page.tsx
@@ -1,17 +1,30 @@
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import {
+    ArrowRight,
     BookCopy,
     BookMarked,
+    Bookmark,
     Box,
     CreditCard,
     Dog,
     Gift,
+    Heart,
     Pencil,
+    Play,
     Plus,
     Search,
     TreeDeciduous,
 } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 const TOPICS = [
     {
@@ -60,33 +73,45 @@ const GIFTS = [
 const VIDEOS = [
     {
         id: 1,
-        title: "Cultivating the farm",
-        tags: ["farming"],
+        title: "Frogs on lilypads",
+        author: "Dobby the elf, age 4",
+        background: "bg-jas-pink",
+        image: "/frog.png",
     },
     {
         id: 2,
-        title: "Cultivating the farm",
-        tags: ["farming"],
+        title: "Duck duck goose",
+        author: "Severus Snape, age 5",
+        background: "bg-jas-green",
+        image: "/goose.png",
     },
     {
         id: 3,
-        title: "Cultivating the farm",
-        tags: ["farming"],
+        title: "Bread and butter",
+        author: "Ron Weasly, age 3",
+        background: "bg-jas-purple",
+        image: "/bread.png",
     },
     {
         id: 4,
-        title: "Cultivating the farm",
-        tags: ["farming", "farming"],
+        title: "Cows in the field",
+        author: "Harry Potter, age 6",
+        background: "bg-jas-purple",
+        image: "/cow.png",
     },
     {
         id: 5,
-        title: "Cultivating the farm",
-        tags: ["farming", "farming"],
+        title: "Riding horses",
+        author: "Albus Dumbledore, age 7",
+        background: "bg-jas-blue",
+        image: "/horse.png",
     },
     {
         id: 6,
-        title: "Cultivating the farm",
-        tags: ["farming"],
+        title: "Bunnies in the barn",
+        author: "Lily Potter, age 4",
+        background: "bg-jas-pink",
+        image: "/bunny.png",
     },
 ];
 
@@ -136,27 +161,111 @@ const Page = () => {
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-3 gap-x-8 gap-y-8">
-                        {VIDEOS.map((video) => (
-                            <div className="relative rounded-xl border-2 border-gray-200 bg-white p-4">
-                                <div className="absolute -top-[8%] left-[50%] h-8 w-32 -translate-x-1/2 transform bg-gray-300" />
-                                <div className="w-74 flex-center h-40 max-h-40 bg-gray-200">
-                                    video
-                                </div>
+                    <div className="grid grid-cols-3 gap-x-8 gap-y-12">
+                        {VIDEOS.map((video, index) => (
+                            <Dialog>
+                                <DialogTrigger>
+                                    <div className="relative rounded-xl border-2 border-gray-200 bg-white p-4">
+                                        <div
+                                            className={cn(
+                                                "absolute -top-[8%] left-[50%] h-8 w-32 -translate-x-1/2 transform",
+                                                index % 3 == 0
+                                                    ? "bg-jas-pink/30"
+                                                    : index % 3 == 1
+                                                      ? "bg-jas-green/30"
+                                                      : "bg-jas-blue/30",
+                                            )}
+                                        />
+                                        <div
+                                            className={cn(
+                                                "w-74 flex-center relative h-40 max-h-40 rounded-xl",
+                                                video.background,
+                                            )}
+                                        >
+                                            <img
+                                                src={video.image}
+                                                className="absolute bottom-0 h-[90%]"
+                                            />
+                                        </div>
 
-                                <div className="space-y-2 text-center">
-                                    <h4 className="mt-4 text-xl font-bold text-black">
-                                        {video.title}
-                                    </h4>
-                                    <div className="flex-center mx-auto flex-row space-x-2">
-                                        {video.tags.map((tag) => (
-                                            <p className="w-fit rounded-full bg-gray-200 px-2 py-1 text-sm font-medium leading-relaxed tracking-wide text-zinc-800">
-                                                {tag}
-                                            </p>
-                                        ))}
+                                        <div className="text-center">
+                                            <h4 className="mt-4 text-xl font-bold text-black">
+                                                {video.title}
+                                            </h4>
+                                            <div className="flex-center mx-auto flex-row space-x-2">
+                                                <p className="w-fit rounded-full px-2 py-1 text-sm font-medium leading-relaxed tracking-wide text-[#7b7b7b]">
+                                                    {video.author}
+                                                </p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle className="mx-auto space-y-4 pt-8 text-center">
+                                            <p className="text-3xl">
+                                                Cultivating the farm
+                                            </p>
+                                            <div className="flex-center mx-auto gap-2">
+                                                <p className="w-fit rounded-full bg-[#EAEAEA] px-2 py-1 text-sm font-medium leading-relaxed tracking-wide text-[#393939]">
+                                                    farming
+                                                </p>
+                                                <p className="w-fit rounded-full bg-[#EAEAEA] px-2 py-1 text-sm font-medium leading-relaxed tracking-wide text-[#393939]">
+                                                    cultivation
+                                                </p>
+                                            </div>
+
+                                            <p className="text-base font-medium text-[#7b7b7b]">
+                                                A story about resilience and
+                                                inspiration as a person
+                                                cultivates his own garden
+                                            </p>
+                                        </DialogTitle>
+                                        <DialogDescription>
+                                            <div className="border-jas-outline flex-between my-4 rounded-xl border-4 bg-white p-4">
+                                                <div className="flex items-center space-x-2">
+                                                    <img src="/dobby.png" />
+                                                    <div>
+                                                        <p className="font-semibold">
+                                                            author
+                                                        </p>
+                                                        <p className="text-jas-gray text-xl font-bold">
+                                                            Dobby the Elf
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                                <div className="bg-jas-outline w-fit rounded-xl p-3">
+                                                    <ArrowRight />
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <img
+                                                    src="/fish.png"
+                                                    className="w-full"
+                                                />
+                                            </div>
+
+                                            <div className="flex-between flex items-center px-8 py-4">
+                                                <div className="rounded-full border-[3px] border-[#c2c2c2] p-3">
+                                                    <Heart className="fill-black text-black" />
+                                                </div>
+                                                <div>
+                                                    <Button className="bg-jas-blue hover:bg-jas-blue/80 h-full space-x-2 rounded-full px-12 py-3">
+                                                        <Play className="fill-white" />
+                                                        <p className="text-xl">
+                                                            Play Story
+                                                        </p>
+                                                    </Button>
+                                                </div>
+                                                <div className="rounded-full border-[3px] border-[#c2c2c2] p-3">
+                                                    <Bookmark className="fill-black text-black" />
+                                                </div>
+                                            </div>
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                </DialogContent>
+                            </Dialog>
                         ))}
                     </div>
                 </div>
