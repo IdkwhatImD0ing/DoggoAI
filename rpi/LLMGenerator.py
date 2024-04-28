9c5b612f4e3e29ff31c8354abcfca0813f1dfe11
Bill Zhang <billzhangsc@gmail.com>
2024-04-28 12:21:07 -0700

    Adding fine tune

---
+++ rpi/LLMGenerator.py
@@ -0,0 +1,89 @@
+import asyncio
+from openai import AsyncOpenAI
+from utils import text_chunker
+
+
+class LLMGenerator:
+    def __init__(
+        self, interruption_queue, sentence_queue, history, stop_event, video_emotions
+    ):
+        self.interruption_queue = interruption_queue
+        self.sentence_queue = sentence_queue
+        self.client = AsyncOpenAI()
+        self.history = history
+        self.stop_event = stop_event  # asyncio.event
+        self.current_task = None
+        # audio_emotions are put into the interruption_queue
+        self.video_emotions = video_emotions
+
+    def start(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.process_items())
+
+    async def process_items(self):
+        while True:
+            while self.stop_event.is_set():
+                if self.current_task and not self.current_task.done():
+                    self.current_task.cancel()
+
+                if not self.interruption_queue.empty():
+                    self.interruption_queue.get_nowait()
+                    self.interruption_queue.task_done()
+
+                await asyncio.sleep(1)
+
+            get_task = asyncio.create_task(self.interruption_queue.get())
+            stop_task = asyncio.create_task(self.stop_event.wait())
+
+            # Wait for both the interruption queue to have an item and the stop event to be set
+            done, pending = await asyncio.wait(
+                [get_task, stop_task], return_when=asyncio.FIRST_COMPLETED
+            )
+
+            if get_task in done:
+                result = get_task.result()
+                self.interruption_queue.task_done()
+                # TODO: integrate the emotions with the transcript
+                item = result["transcript"]
+                formatted_emotions = "Video Emotions: \n"
+                for emotion in self.video_emotions:
+                    if emotion:
+                        formatted_emotions += (
+                            f"{emotion['emotion']} ({emotion['score']:.2%})\n"
+                        )
+
+                emotions = result["audio_emotions"] + "\n" + formatted_emotions
+                # print("LLMGenerator - Emotions:", emotions)
+                self.current_task = asyncio.create_task(
+                    self.process_llm(item, emotions)
+                )
+
+            # Handle tasks that are completed
+            if stop_task in done:
+                print("LLM Generator - Stop event was triggered.")
+                # Handle the stop event
+                if self.current_task and not self.current_task.done():
+                    print("Cancelling LLM task due to stop event.")
+                    self.current_task.cancel()
+                    print("LLM task cancelled.")
+                # Clear the stop event after handling it
+
+            for task in pending:
+                task.cancel()  # Cancel any remaining pending tasks
+
+    async def process_llm(self, text, emotions):
+        response = await self.client.chat.completions.create(
+            model="gpt-4-turbo",
+            stream=True,
+            max_tokens=500,
+            messages=self.history + [{"role": "user", "content": text + emotions}],
+        )
+        text_generator = text_chunker(response)
+        async for chunk in text_generator:
+            if self.stop_event.is_set():
+                while not self.sentence_queue.empty():
+                    self.sentence_queue.get_nowait()
+                    self.sentence_queue.task_done()
+                break
+            await self.sentence_queue.put(chunk)
+            print("LLMGenerator - Putting chunk in sentence queue:", chunk)
---
+++ rpi/VoiceActivityDetector.py
@@ -0,0 +1,205 @@
+import pyaudio
+import numpy as np
+import threading
+import collections
+import wave
+import io
+import asyncio
+import base64
+from concurrent.futures import ThreadPoolExecutor
+from openai import AsyncOpenAI
+import json
+
+
+class VoiceActivityDetector:
+    def __init__(
+        self,
+        interruption_queue,
+        loop,
+        stop_event,
+        history,
+        hume_socket,
+        server_socket,
+        rate=16000,
+        format=pyaudio.paInt16,
+        channels=1,
+        chunk_size=512,
+        silence_threshold=200,
+    ):
+        self.loop = loop
+        self.stop_event = stop_event
+        self.history = history
+        self.hume_socket = hume_socket
+        self.server_socket = server_socket
+        self.interruption_queue = interruption_queue  # Interruption Queue
+        self.rate = rate
+        self.format = format
+        self.channels = channels
+        self.chunk_size = chunk_size
+        self.silence_threshold = silence_threshold
+        self.count = 0
+
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(
+            rate=self.rate,
+            format=self.format,
+            channels=self.channels,
+            input=True,
+            frames_per_buffer=self.chunk_size,
+        )
+        self.audio_buffer = collections.deque(
+            maxlen=int((self.rate // self.chunk_size) * 0.5)
+        )
+        self.frames = []
+        self.long_term_noise_level = 0.0
+        self.current_noise_level = 0.0
+        self.voice_activity_detected = False
+        self.is_recording = False
+        self.executor = ThreadPoolExecutor(max_workers=1)
+
+        self.client = AsyncOpenAI()
+
+    def get_levels(self, data):
+        pegel = np.abs(np.frombuffer(data, dtype=np.int16)).mean()
+        self.long_term_noise_level = self.long_term_noise_level * 0.995 + pegel * (
+            1.0 - 0.995
+        )
+        self.current_noise_level = self.current_noise_level * 0.920 + pegel * (
+            1.0 - 0.920
+        )
+        return pegel, self.long_term_noise_level, self.current_noise_level
+
+    def start_recording(self):
+        self.is_recording = True
+        self.thread = threading.Thread(target=self.record)
+        self.thread.start()
+
+    def stop_recording(self):
+        self.is_recording = False
+        self.thread.join()
+
+    def record(self):
+        ambient_noise_level = 0
+        print("Start speaking.")
+        while self.is_recording:
+            data = self.stream.read(self.chunk_size, exception_on_overflow=False)
+            pegel, long_term_noise_level, current_noise_level = self.get_levels(data)
+            self.audio_buffer.append(data)
+
+            if (
+                not self.voice_activity_detected
+                and current_noise_level > long_term_noise_level + self.silence_threshold
+            ):
+                self.voice_activity_detected = True
+                print("Listening.")
+                self.stop_event.set()
+                ambient_noise_level = long_term_noise_level
+                self.frames.extend(list(self.audio_buffer))
+
+            if self.voice_activity_detected:
+                self.frames.append(data)
+                if current_noise_level < ambient_noise_level + 100:
+                    print("Stopped listening.")
+                    self.stop_event.clear()
+                    self.send_voice_data(b"".join(self.frames))
+                    self.frames.clear()
+                    self.voice_activity_detected = False
+
+    def send_voice_data(self, audio_data):
+        # Transcribe audio data asynchronously and add to queue
+        print("Transcribing...")
+        asyncio.run_coroutine_threadsafe(
+            self.transcribe_and_enqueue(audio_data), self.loop
+        )
+
+    async def transcribe_and_enqueue(self, audio_data):
+        try:
+            wav_buffer = io.BytesIO()
+            with wave.open(wav_buffer, "wb") as wf:
+                wf.setnchannels(self.channels)
+                wf.setsampwidth(self.audio.get_sample_size(self.format))
+                wf.setframerate(self.rate)
+                wf.writeframes(audio_data)
+            wav_data = wav_buffer.getvalue()
+
+            # Save to file
+            # with open(f"input{self.count}.wav", "wb") as f:
+            #     self.count +=1
+            #     f.write(wav_data)
+            audio_stream = io.BytesIO(wav_data)
+            audio_stream.name = "audio.wav"
+
+            # Save as wav file
+            with open("audio.wav", "wb") as f:
+                f.write(wav_data)
+
+            transcript_task = self.client.audio.transcriptions.create(
+                model="whisper-1",
+                file=audio_stream,
+                response_format="text",
+                language="en",
+            )
+
+            # base64 encode wav
+            base64_audio = base64.b64encode(wav_data).decode()
+
+            audio_task = self.process_hume(base64_audio)
+
+            transcript, audio_emotions = await asyncio.gather(
+                transcript_task, audio_task
+            )
+            # print("VoiceActivityDetector - Emotions:", audio_emotions)
+
+            # TODO: pass audio and video to GPT
+            emotion_context = {
+                "event": "user",
+                "transcript": transcript,
+                "audio_emotions": audio_emotions,
+            }
+
+            await self.server_socket.send(json.dumps(emotion_context))
+
+            pretty_emotions = "Voice Emotions:\n"
+            for i, emotion in enumerate(audio_emotions):
+                pretty_emotions += (
+                    f"Emotion {i + 1}: {emotion['name']} ({emotion['score']:.2%})\n"
+                )
+
+            emotion_context = {
+                "transcript": transcript,
+                "audio_emotions": pretty_emotions,
+            }
+
+            await self.interruption_queue.put(emotion_context)
+            self.history.append({"role": "user", "content": transcript})
+            print("VoiceActivityDetector - Added to queue:", transcript)
+
+        except Exception as e:
+            print("Failed to transcribe:", str(e))
+
+    async def process_hume(self, audio_base64):
+        data = {"data": audio_base64, "models": {"prosody": {}}}
+
+        await self.hume_socket.send(json.dumps(data))
+
+        response = await self.hume_socket.recv()
+        response = json.loads(response)
+
+        # * Emotions has {"name": "anger", "score": 0.5}
+        emotions = (
+            response.get("prosody", {}).get("predictions", [])[0].get("emotions", [])
+        )
+        # Find top 3 emotions and format them
+        emotions = sorted(emotions, key=lambda e: e["score"], reverse=True)[:3]
+        audio_emotions = []
+        for emotion in emotions:
+            audio_emotions.append(
+                {"emotion": emotion["name"], "score": emotion["score"]}
+            )
+
+        return audio_emotions
+
+    def close(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
